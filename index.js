bc71e1c16844784222fcd6267c6ef9575feac127
xhaqur xhariph <shakuritoshariff@gmail.com>
2024-06-29 15:50:18 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(){
+    let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return drivers.slice(0, 2);
+}
+returnFirstTwoDrivers();
+
+const returnLastTwoDrivers = function (){
+    let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.slice(2);
+}
+returnLastTwoDrivers();
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(integer){
+    return function(){
+        return (`${integer}` * 5);
+    }
+}
+createFareMultiplier(5);
+
+const fareDoubler = function(fare){ 
+    return fare * 2
+}
+fareDoubler(10);
+
+const fareTripler = function(fare){
+    return fare * 3
+}
+fareTripler(12);
+
+function selectDifferentDrivers(drivers, selectingDrivers){
+    return selectingDrivers(drivers);
+}
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
